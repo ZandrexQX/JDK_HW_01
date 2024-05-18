8cf11fd2527607c411944a41b3df0d1c89514256
Zandrex <zandrex@mail.ru>
2024-05-18 19:46:06 +0300

    git init

---
+++ ServerWindow.java
@@ -0,0 +1,13 @@
+package chat.server;
+
+import javax.swing.*;
+
+public class ServerWindow extends JFrame {
+    private static final int WINDOW_HEIGHT = 500;
+    private static final int WINDOW_WIDTH = 550;
+    private static final int WINDOW_POSX = 800;
+    private static final int WINDOW_POSY = 300;
+
+    private JButton btnStart = new JButton("Запуск");
+    private JButton btnStop = new JButton("Остановка");
+}
